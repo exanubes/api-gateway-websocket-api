d06f145192c74c10bb615ed3e672842bbdf50063
Exanubes <blog@exanubes.com>
2024-07-05 21:25:56 +0200

    Add dynamodb table

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import * as aws from "@pulumi/aws";
 
+new aws.dynamodb.Table('connections', {
+    name: 'WS_CONNECTIONS',
+    attributes: [
+        { name: 'connectionId', type: 'S' },
+    ],
+    billingMode: 'PAY_PER_REQUEST',
+    hashKey: 'connectionId',
+});
+
 const api = new aws.apigatewayv2.Api('websocket-api', {
     name: 'exanubes-websocket-api',
     protocolType: 'WEBSOCKET',
