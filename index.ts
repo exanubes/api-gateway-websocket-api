b987de0075f7bf2aae403bab40e97c3498933fd0
Exanubes <blog@exanubes.com>
2024-07-05 21:25:41 +0200

    Add api gateway

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const api = new aws.apigatewayv2.Api('websocket-api', {
+    name: 'exanubes-websocket-api',
+    protocolType: 'WEBSOCKET',
+    routeSelectionExpression: '$request.body.type'
+});
